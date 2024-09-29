8c3b61521c15910dde34abebe276d3bd1a7d1f20
deepanshu <krrohan129@gmail.com>
2024-09-30 00:12:14 +0530

    completed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/https---github.com-Deepanshu00-1-Hacknomania-Final-project/"
 })
